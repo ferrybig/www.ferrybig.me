70e916b2cd0b6810cfa01619db4c591dc5bc7d52
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2022-03-26 22:42:00 +0100

    New blog page

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -2,5 +2,12 @@ import * as myNewWebsite from './my-new-website/index.md';
 import * as githubBackup from './github-backup/index.md';
 import * as reactBareBonesSetup from './react-bare-bones-setup/index.md';
 import * as webpackLoader from './webpack-loader/index.md';
+import * as howIFoundATypeScriptPublicPlaygroundXssExploit from './how-i-found-a-typescript-public-playground-xss-exploit/index.md';
 
-export default [myNewWebsite, githubBackup, reactBareBonesSetup, webpackLoader];
+export default [
+	myNewWebsite,
+	githubBackup,
+	reactBareBonesSetup,
+	webpackLoader,
+	howIFoundATypeScriptPublicPlaygroundXssExploit,
+];
