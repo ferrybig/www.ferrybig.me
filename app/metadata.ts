9700c26ae3d97bc8ad66eaf96e14c6d7970f699c
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2025-08-04 18:26:31 +0200

    Rebrand to ferrybig.nl

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,5 +1,5 @@
-export const SITE_URL = 'https://www.ferrybig.me/';
-export const SITE_TITLE = 'Ferrybig.me';
+export const SITE_URL = 'https://www.ferrybig.nl/';
+export const SITE_TITLE = 'Ferrybig.nl';
 export const DEFAULT_TABLE_OF_CONTENTS_MAX_DEPTH = 4;
 export const BASE_DIR = 'public/';
 export const GIT_BRANCH = 'master';
